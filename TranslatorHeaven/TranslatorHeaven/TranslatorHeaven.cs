cb6f9c411308d93362dffed165b2af20cc6e965e
NK-Hertz <fester.s.son@gmail.com>
2014-03-28 23:21:20 +0200

    init commit

---
+++ TranslatorHeaven/TranslatorHeaven/TranslatorHeaven.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslatorHeaven
+{
+    class TranslatorHeaven
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
