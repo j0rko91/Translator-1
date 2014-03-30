a04d4af21040a427f290eeccbc728bf2d965087c
NK-Hertz <fester.s.son@gmail.com>
2014-03-30 18:40:51 +0300

    read, make copy, try to compile

--- TranslatorHeaven/TranslatorHeaven/TranslatorHeaven.cs
+++ TranslatorHeaven/TranslatorHeaven/TranslatorHeaven.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Diagnostics;
 
 namespace TranslatorHeaven
 {
@@ -10,12 +9,43 @@ namespace TranslatorHeaven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dsadasd");
-            Console.WriteLine("Digame lvl");
+            //reading file
+            string fileToRead = ("../../Test/example.scs");
+            string[] inputFile = File.ReadAllLines(fileToRead);
+            int inputFileLines = File.ReadAllLines(fileToRead).Length;
+            
+            //user can input the end-result file`s name
+            Console.WriteLine("Enter name for the new file: ");
+            string fileName = Console.ReadLine();
+            if (string.IsNullOrEmpty(fileName) || string.IsNullOrWhiteSpace(fileName))
+            {
+                fileName = "result";
+            }
+
+            //making a copy in a .cs format
+            using (StreamWriter writer = new StreamWriter("../../Test/"+ fileName + ".cs"))
+            {
+                for (int line = 0; line < inputFileLines; line++)
+                {
+                    writer.WriteLine(inputFile[line]);
+                }
+            }
+
+            string fileToCompile = "../../Test/" + fileName + ".cs";
+            string output = string.Format("{0}.exe", fileName);
+
+            //doesnt create file
+            CodeDomProvider codeProvider = CodeDomProvider.CreateProvider("CSharp");
+            ICodeCompiler icc = codeProvider.CreateCompiler();
+            System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = output;
+            CompilerResults compileResults = codeProvider.CompileAssemblyFromFile(parameters, fileToCompile);
 
 
-            Console.WriteLine("dsaidjasidjsa");
-            Console.ReadLine();
+            Console.WriteLine();
+            Process.Start(output);
+            
         }
     }
 }
